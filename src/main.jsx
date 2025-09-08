8d0230e2120a3e9db9bcf8e7c7c294d523006969
info-EE <info@europaenvios.com>
2025-09-08 20:23:40 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
-// Import explícito y tolerante (default o named)
-import AppDefault, { App as AppNamed } from "./App.jsx";
-const RootApp = AppDefault ?? AppNamed;
-
-if (!RootApp) {
-  // Si volviera a fallar, verás este error en consola en vez de quedar pantalla blanca.
-  throw new Error(
-    'No se encontró el componente App. Asegúrate de tener `export default App` al final de src/App.jsx (o `export const App = ...`).'
-  );
-}
+import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RootApp />
+    <App />
   </React.StrictMode>
 );
